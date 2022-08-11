448aaa5526ee7fbcb09beae7617d42dd47889916
erokhiin <33400447+erokhiin@users.noreply.github.com>
2022-08-11 13:55:54 +0400

    add systems: map_renderer, entity_renderer and player_input

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,5 +1,12 @@
+mod player_input;
+mod map_render;
+mod entity_renderer;
 use crate::prelude::*;
 
 pub fn build_scbheduler() -> Schedule {
-    Schedule::builder().build()
+    Schedule::builder()
+        .add_system(player_input::player_input_system())
+        .add_system(map_render::map_render_system())
+        .add_system(entity_renderer::entity_renderer_system())
+        .build()
 }
