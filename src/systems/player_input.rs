a341be52300c1f4f04e069f5c5873fb1c6443370
erokhiin <33400447+erokhiin@users.noreply.github.com>
2022-08-21 18:18:23 +0400

    add random move and turn state

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -8,6 +8,7 @@ pub fn player_input(
     #[resource] map: &Map,
     #[resource] key: &Option<VirtualKeyCode>,
     #[resource] camera: &mut Camera,
+    #[resource] turn_state: &mut TurnState,
 ) {
     if let Some(key) = key {
         let delta = match key {
@@ -18,16 +19,15 @@ pub fn player_input(
             _ => Point::new(0, 0),
         };
 
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query().filter(component::<Player>());
+        let mut players = <&mut Point>::query().filter(component::<Player>());
 
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                }
-            });
-        }
+        players.iter_mut(ecs).for_each(|pos| {
+            let destination = *pos + delta;
+            if map.can_enter_tile(destination) {
+                *pos = destination;
+                camera.on_player_move(destination);
+                *turn_state = TurnState::PlayerTurn;
+            }
+        });
     }
 }
