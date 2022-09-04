05b2fbdd4f1af870c831503301e78289a82f4f0f
erokhiin <33400447+erokhiin@users.noreply.github.com>
2022-09-04 13:53:36 +0400

    implementing intent messages system 🎋

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -1,11 +1,11 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(MovingRandomly)]
-pub fn random_move(esc: &mut SubWorld, #[resource] map: &Map) {
-    let mut movers = <(&mut Point, &MovingRandomly)>::query();
-    movers.iter_mut(esc).for_each(|(pos, _)| {
+pub fn random_move(esc: &SubWorld, commands: &mut CommandBuffer) {
+    let mut movers = <(Entity, &Point, &MovingRandomly)>::query();
+    movers.iter(esc).for_each(|(entity, pos, _)| {
         let mut rng = RandomNumberGenerator::new();
         let destinations = match rng.range(0, 4) {
             0 => Point::new(-1, 0),
@@ -14,8 +14,12 @@ pub fn random_move(esc: &mut SubWorld, #[resource] map: &Map) {
             _ => Point::new(0, 1),
         } + *pos;
 
-        if map.can_enter_tile(destinations) {
-            *pos = destinations;
-        }
+        commands.push((
+            (),
+            WantsToMove {
+                entity: *entity,
+                destination: destinations,
+            },
+        ));
     })
 }
