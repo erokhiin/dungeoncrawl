ce08c26b68f0914c697365e5d83b61b5b86ad88b
erokhiin <33400447+erokhiin@users.noreply.github.com>
2022-09-17 12:14:11 +0400

    add intend to attack to random move system

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -3,23 +3,58 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(MovingRandomly)]
-pub fn random_move(esc: &SubWorld, commands: &mut CommandBuffer) {
+#[read_component(Health)]
+#[read_component(Player)]
+pub fn random_move(ecs: &SubWorld, commands: &mut CommandBuffer) {
     let mut movers = <(Entity, &Point, &MovingRandomly)>::query();
-    movers.iter(esc).for_each(|(entity, pos, _)| {
+    let mut positions = <(Entity, &Point, &Health)>::query();
+
+    movers.iter(ecs).for_each(|(mover, pos, _)| {
         let mut rng = RandomNumberGenerator::new();
-        let destinations = match rng.range(0, 4) {
+
+        let delta = match rng.range(0, 4) {
             0 => Point::new(-1, 0),
             1 => Point::new(1, 0),
             2 => Point::new(0, -1),
             _ => Point::new(0, 1),
-        } + *pos;
+        };
+        let destination = delta + *pos;
 
-        commands.push((
-            (),
-            WantsToMove {
-                entity: *entity,
-                destination: destinations,
-            },
-        ));
+        let is_mover_do_something = delta.x != 0 || delta.y != 0;
+
+        if is_mover_do_something {
+            let mut hit_someone = false;
+
+            positions
+                .iter(ecs)
+                .filter(|(_, pos, _)| **pos == destination)
+                .for_each(|(victim, _, _)| {
+                    if ecs
+                        .entry_ref(*victim)
+                        .unwrap()
+                        .get_component::<Player>()
+                        .is_ok()
+                    {
+                        commands.push((
+                            (),
+                            WantsToAttack {
+                                attacker: *mover,
+                                victim: *victim,
+                            },
+                        ));
+                    }
+                    hit_someone = true; // This prevents the monster from moving into a tile containing another monster
+                });
+
+            if !hit_someone {
+                commands.push((
+                    (),
+                    WantsToMove {
+                        entity: *mover,
+                        destination,
+                    },
+                ));
+            }
+        }
     })
 }
