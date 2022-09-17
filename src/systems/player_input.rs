8af39205a52c8d3289a3d714bc3aa2d5f493495d
erokhiin <33400447+erokhiin@users.noreply.github.com>
2022-09-17 11:08:32 +0400

    add WantsToAttack component

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -23,17 +23,42 @@ pub fn player_input(
         };
 
         let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
 
-        players.iter(ecs).for_each(|(entity, pos)| {
-            let destination = *pos + delta;
-            commands.push((
-                (),
-                WantsToMove {
-                    entity: *entity,
-                    destination,
-                },
-            ));
-            *turn_state = TurnState::PlayerTurn;
-        });
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
+
+        let is_player_do_something = delta.x != 0 || delta.y != 0;
+
+        if is_player_do_something {
+            let mut hit_something = false;
+            
+            enemies
+                .iter(ecs)
+                .filter(|(_, pos)| **pos == destination)
+                .for_each(|(entity, _)| {
+                    hit_something = true;
+
+                    commands.push((
+                        (),
+                        WantsToAttack {
+                            attacker: player_entity,
+                            victim: *entity,
+                        },
+                    ));
+                });
+            
+            if !hit_something {
+                commands
+                .push(((), WantsToMove {
+                        entity: player_entity,
+                        destination,
+                    },
+                ));
+            }
+        }
+        *turn_state = TurnState::PlayerTurn;
     };
 }
