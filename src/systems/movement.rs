dc1f497a984666709209ed64658fcf4d7fd27907
erokhiin <33400447+erokhiin@users.noreply.github.com>
2023-05-15 23:24:27 +0400

    add spatial memory 🗺️

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -15,13 +15,16 @@ pub fn movement(
     if let Ok(entry) = ecs.entry_ref(want_move.entity) {
         if let Ok(fov) = entry.get_component::<FieldOfView>() {
             commands.add_component(want_move.entity, fov.clone_dirty());
-        }
 
-        if entry.get_component::<Player>().is_ok() {
-            camera.on_player_move(want_move.destination);
+            if entry.get_component::<Player>().is_ok() {
+                camera.on_player_move(want_move.destination);
+                fov.visible_tiles.iter().for_each(|pos| {
+                    map.revealed_tiles[map_idx(pos.x, pos.y)] = true;
+                })
+            }
         }
     }
-
+    
     if map.can_enter_tile(want_move.destination) {
         commands.add_component(want_move.entity, want_move.destination);
 
